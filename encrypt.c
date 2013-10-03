ee918b036a898bbdef2a66c7f904ddbcd78aabe3
merrick.heley <merrick.heley@gmail.com>
2013-10-03 21:05:47 +1000

    Added encryption files and .project to gitignore

---
+++ encrypt.c
@@ -0,0 +1,9 @@
+/*
+ *  encrypt.h
+ *  Ext3301 File System
+ *
+ *  Merrick Heley (merrick.heley@uqconnect.edu.au)
+ *
+ *  Implements a naive encryption scheme for folders stored in the /encrypt
+ *  directory of the ext3301 file system.
+ */
