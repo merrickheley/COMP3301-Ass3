2b1edb7a65ad8503c097cc2fdd333296c3f1fe7a
merrick.heley <merrick.heley@gmail.com>
2013-10-11 01:23:05 +1000

    Added dedecrypt and tested

--- encrypt.c
+++ encrypt.c
@@ -55,7 +55,7 @@ ssize_t do_sync_encrypt_write(struct file *filp, const char __user *buf,
     /* File is within the encrypt dir */
     if (strncmp(folder->d_name.name, ENCRYPT_DIR, strlen(ENCRYPT_DIR)+1) == 0) {
 
-        ext2_msg(sb, KERN_DEBUG, "%s resides in /encrypt", filp->f_dentry->d_name.name);
+        ext2_msg(sb, KERN_DEBUG, "%s resides in /encrypt. Encrypting.", filp->f_dentry->d_name.name);
 
         /* Encrypt each char in the buffer */
         for (i = 0; i < len; i++) {
@@ -92,5 +92,45 @@ ssize_t do_sync_encrypt_write(struct file *filp, const char __user *buf,
 ssize_t do_sync_encrypt_read(struct file *filp, char __user *buf,
         size_t len, loff_t *ppos) {
 
-    return do_sync_read(filp, buf, len, ppos);
+    struct super_block *sb = filp->f_inode->i_sb;
+    struct dentry *folder = filp->f_dentry;
+    char *decryptedBuf;
+    size_t i = 0;
+    size_t ret;
+    mm_segment_t oldFs;
+
+    /* Allocate memory for the buffer */
+    if ((decryptedBuf = kmalloc(len, GFP_NOFS)) == 0) {
+        ext2_error(sb, KERN_CRIT, "Failed to allocate memory");
+    }
+
+    /* Move to kernel space to avoid read errors */
+    /* decrpytedBuf will be outside threads segmentation limit */
+    oldFs = get_fs();
+    set_fs (KERNEL_DS);
+    ret = do_sync_read(filp, decryptedBuf, len, ppos);
+    set_fs(oldFs);
+
+    /* Iterate up to folder below root */
+    while (strncmp(folder->d_parent->d_name.name, "/", 2) != 0) {
+        folder = folder->d_parent;
+    }
+
+    /* File is within the encrypt dir */
+    if (strncmp(folder->d_name.name, ENCRYPT_DIR, strlen(ENCRYPT_DIR)+1) == 0) {
+
+        ext2_msg(sb, KERN_DEBUG, "%s resides in /encrypt. Decrypting.", filp->f_dentry->d_name.name);
+
+        /* Decrypt each char in the buffer */
+        for (i = 0; i < len; i++) {
+            decryptedBuf[i] ^= encrypt_key;
+        }
+    }
+
+    /* Set the buffer */
+    copy_to_user(__user buf, decryptedBuf, len);
+
+    kfree(decryptedBuf);
+
+    return ret;
 }
