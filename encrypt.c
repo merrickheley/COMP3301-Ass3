066f82dfab7b0f8c02d7621a81b8e5f3605adcad
merrick.heley <merrick.heley@gmail.com>
2013-10-10 21:49:29 +1000

    Added encryption wrapper for the ENCRYPT_DIR

--- encrypt.c
+++ encrypt.c
@@ -10,17 +10,70 @@
 
 #include "encrypt.h"
 
+#include "ext2.h"
+
+#include <asm/uaccess.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 
 __u8 encrypt_key = 0;
 
 /**
  * Encrypted write
  *
+ * Calls do_sync_write, but if a key is set and the file is within
+ * ENCRYPT_DIR the buffer is encrypted with encrypt_key before write.
  *
+ * filp : File to be written to
+ * buf  : Buffer to write to the file
+ * len  : Length of write to the file
+ * ppos : Position of write
  */
 ssize_t do_sync_encrypt_write(struct file *filp, const char __user *buf,
 		size_t len, loff_t *ppos) {
 
-	return do_sync_write(filp, buf, len, ppos);
+    struct super_block *sb = filp->f_inode->i_sb;
+    struct dentry *folder = filp->f_dentry;
+    char *encryptedBuf;
+    size_t i = 0;
+    size_t ret;
+    mm_segment_t oldFs;
+
+    /* Allocate memory for the buffer */
+    if ((encryptedBuf = kmalloc(len, GFP_NOFS)) == 0) {
+        ext2_error(sb, KERN_CRIT, "Failed to allocate memory");
+    }
+
+    /* Get the buffer */
+    copy_from_user(encryptedBuf, __user buf, len);
+
+    /* Iterate up to folder below root */
+    while (strncmp(folder->d_parent->d_name.name, "/", 2) != 0) {
+        folder = folder->d_parent;
+    }
+
+    /* File is within the encrypt dir */
+    if (strncmp(folder->d_name.name, ENCRYPT_DIR, strlen(ENCRYPT_DIR)+1) == 0) {
+
+        ext2_msg(sb, KERN_DEBUG, "%s resides in /encrypt", filp->f_dentry->d_name.name);
+
+        /* Encrypt each char in the buffer */
+        for (i = 0; i < len; i++) {
+            encryptedBuf[i] ^= encrypt_key;
+        }
+
+        /* Move to kernel space to avoid write errors */
+        /* encrpytedBuf will be outside threads segmentation limit */
+        oldFs = get_fs();
+        set_fs (KERNEL_DS);
+        ret = do_sync_write(filp, encryptedBuf, len, ppos);
+        set_fs(oldFs);
+
+    } else {
+        ret = do_sync_write(filp, buf, len, ppos);
+    }
+
+    kfree(encryptedBuf);
+
+    return ret;
 }
